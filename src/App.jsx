f2db97ebfbc4b7b2fe8efca1e7c7b02f6ed91030
AnthoniaEfe <anthoniaefe36@gmail.com>
2025-04-11 00:15:35 +0100

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <nav className='flex'>
+      <img src="./assets/images/logo.svg" alt="sneakers logo"/> 
+      
+      Collections
+  Men
+  Women
+  About
+  Contact
+
+  Sneaker Company</nav>
+
+<section>
+
+  Fall Limited Edition Sneakers
+
+  These low-profile sneakers are your perfect casual wear companion. Featuring a 
+  durable rubber outer sole, they’ll withstand everything the weather can offer.
+
+  $125.00
+  50%
+  $250.00
+
+  0
+  Add to cart
+</section>
+
+    </>
+  )
+}
+
+export default App
